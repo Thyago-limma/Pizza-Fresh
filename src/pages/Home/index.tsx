2969c13abfb5ffb0ee73f3128cdc47d3a03dafe9
Thyago-limma <102415139+Thyago-limma@users.noreply.github.com>
2022-09-29 21:05:25 -0300

    .

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,41 @@
 import Menu from "components/Menu";
+import { DateTime } from "luxon";
 import { ReactComponent as Search } from "assets/icons/search.svg";
 import * as S from "./style";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
-import { DateTime } from "luxon";
 import ProductItemList from "components/ProductItemList";
 import ProductItem from "components/ProductItem";
 import OrderDetails from "components/OrderDetails";
+import Overlay from "components/Overlay";
+import CheckoutSection from "components/CheckoutSection";
+import { useNavigate } from "react-router-dom";
+import { products } from "mocks/products";
+import { orders } from "mocks/orders";
+import { ProductResponse } from "types/Product";
+import { OrderType } from "types/orderType";
+import { useState } from "react";
 
 const Home = () => {
   const dateDescription = DateTime.now().toLocaleString({
     ...DateTime.DATE_SHORT,
     weekday: "long",
   });
+  const navigate = useNavigate();
+  const [activeOrderType, setActiverOrderType] = useState(
+    OrderType.COMER_NO_LOCAL
+  );
+  const handleNavigation = (path: RoutePath) => navigate(path);
+  const handleSelection = (product: ProductResponse) => {};
 
   return (
     <S.Home>
-      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <Menu
+        active={RoutePath.HOME}
+        navItems={navigationItems}
+        onNavigate={handleNavigation}
+        onLogout={() => navigate(RoutePath.LOGIN)}
+      />
       <S.HomeContent>
         <header>
           <S.HomeHeaderDetails>
@@ -38,14 +57,28 @@ const Home = () => {
           </S.HomeProductTitle>
           <S.HomeProductList>
             <ProductItemList>
-              <ProductItem />
+              {Boolean(products.length) &&
+                products.map((product, index) => (
+                  <ProductItem
+                    product={product}
+                    key={`ProductItem-${index}`}
+                    onSelect={handleSelection}
+                  />
+                ))}
             </ProductItemList>
           </S.HomeProductList>
         </div>
       </S.HomeContent>
       <aside>
-        <OrderDetails />
+        <OrderDetails
+          orders={orders}
+          onChangeActiveOrderType={(data) => setActiverOrderType(data)}
+          activeOrderType={activeOrderType}
+        />
       </aside>
+      {/* <Overlay>
+                <CheckoutSection />
+            </Overlay> */}
     </S.Home>
   );
 };
